058336603e0bde6a61d281e55f708ce6d390e76a
Konina Elena <e.konina@roboforex.com>
2019-11-20 15:59:13 +0200

    added main page

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/main", name="app_main")
+     */
+    public function homepage()
+    {
+        return $this->render('main/homepage.html.twig');
+
+    }
+}
